47e3c2efc1979eef22e869946243854cd73a6d6c
Sam <samuelrmeyer@gmail.com>
2018-01-27 11:59:03 -0600

    initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Javascript: The Good Parts', pages: 104},
+        {title: 'Harry Potter', pages: 357},
+        {title: 'Dark Tower', pages: 87},
+        {title: 'Eloquent Ruby', pages: 1},
+    ]
+}
